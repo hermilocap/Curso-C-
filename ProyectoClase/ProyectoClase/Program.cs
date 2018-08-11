9488a71b41f706faf3b5ac3875beb8829240d840
hermilocap <hermilocap@hotmail.com>
2018-08-11 17:45:47 -0500

    Programacion orientada en C sharp

---
+++ ProyectoClase/ProyectoClase/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsEmpleados empleado;
+            empleado = new clsEmpleados();
+            empleado.Edad = 25;
+            empleado.Nombre = "Hermilo";
+            empleado.SueldoDiario = 12.5m;
+            //interface-establece los elementos basico de un objeto   para presentarlos  a otro y poder utilizarlo
+
+            decimal total;
+            total = empleado.CalculaSalario(30);
+
+            Console.WriteLine("El mensual del empleado "+empleado.Nombre);
+            Console.WriteLine("es "+total.ToString("C"));
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ ProyectoClase/ProyectoClase/clsEmpleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClase
+{
+    class clsEmpleados
+    {
+        //contructor: es publico no regresa nigun tipo y tiene el mismo nombre de una clase
+        public clsEmpleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.5m;
+            Edad = 0;
+        }
+
+        public string Nombre;
+        public decimal SueldoDiario;
+        public int Edad;
+        public decimal CalculaSalario(int numeroDias) {
+            return SueldoDiario * numeroDias;
+        }
+
+    }
+}
