529aa1136f146e1d938b03d7bb3697abe0fff48e
hermilocap <hermilocap@hotmail.com>
2018-08-12 12:21:33 -0500

    Uso de herencia de clases en c charp

---
+++ Herencia/Herencia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsClientesVentas cliente;
+            cliente = new clsClientesVentas();
+            cliente.IdCliente = 122;
+            cliente.Apellido = "sANTIAGO";
+            cliente.Nombre = "hERMILO";
+            cliente.RFC = "SABH";
+            cliente.Direccion = "Calle apatzingan";
+            cliente.Colonia = "lazaro cardenas";
+            cliente.Municipio = "cuerna";
+            cliente.EsCredito = true;
+            Console.WriteLine(cliente.Apellido+" "+cliente.Nombre);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Herencia/Herencia/clsClientes.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+   public class clsClientes
+    {
+        private int _IdCliente;
+        private string _Nombre;
+        private string _Apellido;
+
+        public int IdCliente
+        {
+            get
+            {
+                return _IdCliente;
+            }
+
+            set
+            {
+                _IdCliente = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value;
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return _Apellido;
+            }
+
+            set
+            {
+                _Apellido = value;
+            }
+        }
+    }
+}
