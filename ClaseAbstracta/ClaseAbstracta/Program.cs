cf69fa63b1c26dd9fb68e0cc18530b5d7560b924
hermilocap <hermilocap@hotmail.com>
2018-08-12 14:46:20 -0500

    Clase abstracta en c charp

--- ClaseAbstracta/ClaseAbstracta/Program.cs
+++ ClaseAbstracta/ClaseAbstracta/Program.cs
@@ -10,6 +10,13 @@ namespace ClaseAbstracta
     {
         static void Main(string[] args)
         {
+            //clase abstracta no se puede instanciar
+            clsClientesBase cliente = new clsClientesBase(0,"hermilo","001","sabh",1,"hermilo santiago");
+            Console.WriteLine(cliente.Clave+" "+cliente.Nombre);
+            Console.WriteLine(cliente.TipoRegimen);
+            Console.WriteLine(cliente.RFC);
+           
+            Console.ReadKey();
         }
     }
 }
