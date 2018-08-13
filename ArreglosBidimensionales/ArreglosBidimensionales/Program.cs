a603e0e1da0700a651daeda40a2f65bd32d17e1d
hermilocap <hermilocap@hotmail.com>
2018-08-12 19:19:00 -0500

    Arreglos bidimensionaless

---
+++ ArreglosBidimensionales/ArreglosBidimensionales/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosBidimensionales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMatrices matriz = new clsMatrices();
+            matriz.InicializarMatriz();
+            matriz.ImprimirMatriz();
+        }
+    }
+}
---
+++ ArreglosBidimensionales/ArreglosBidimensionales/clsMatrices.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArreglosBidimensionales
+{
+    class clsMatrices
+    {
+        private int[,] Matriz;
+        public clsMatrices() {
+            Matriz = new int[4, 2];
+
+        }
+        public void InicializarMatriz()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Matriz[i, j] = 1;
+
+                }
+            }
+
+        }
+        public void ImprimirMatriz()
+        {
+            //filas
+            for (int i = 0; i < 4; i++)
+            {
+                //columnas
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write("{0}", Matriz[i,j]);
+                    
+
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
