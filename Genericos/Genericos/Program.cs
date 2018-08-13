170e63abe2e91095cc2ff58ec95752e2c9953a5f
hermilocap <hermilocap@hotmail.com>
2018-08-12 21:20:26 -0500

    Uso genericos

---
+++ Genericos/Genericos/Program.cs
@@ -0,0 +1,53 @@
+﻿using ClaseAbstracta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcion = "";
+            List<clsClientesBase> clientes;
+            clientes = new List<clsClientesBase>();
+
+            while(opcion!="s")
+            {
+                Console.WriteLine("Opciones");
+                Console.WriteLine("a. Agregar a la lista");
+                Console.WriteLine("b. Quitar de la lista");
+                Console.WriteLine("c. Imprimir lista");
+                Console.WriteLine();
+                Console.WriteLine("s. Salir");
+                opcion = Console.ReadLine();
+                if (opcion == "a")
+                {
+                    string cadena = Console.ReadLine();
+                    clsClientesBase cliente = new clsClientesBase();
+                    cliente.Id = clientes.Count + 1;
+                    cliente.Nombre = cadena;
+                    clientes.Add(cliente);
+                }
+                else if (opcion == "b")
+                {
+                    string cadena = Console.ReadLine();
+                    clientes.RemoveAt(Convert.ToInt32(cadena));
+
+                }
+                else if(opcion=="c") {
+                    foreach (clsClientesBase item in clientes)
+                    {
+                        Console.WriteLine("Los datos del cliente son");
+                        Console.WriteLine(item.Id.ToString()+" "+item.Nombre);
+                    }
+                }
+
+                }
+            }
+        }
+    }
+
---
+++ Genericos/Genericos/clsClientesBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstracta
+{
+   public class clsClientesBase:clsAbsCliente
+    {
+        public clsClientesBase()
+        {
+            Id = 0;
+            Nombre = string.Empty;
+            Clave= string.Empty;
+            RFC= string.Empty;
+            TipoRegimen= 0;
+            NombreContacto= string.Empty;
+        }
+
+        public clsClientesBase(int pId,string pNombre, string pClave, string pRFC, int PTipoRegimen, string pNombreComplento)
+        {
+            Id = pId;
+            Nombre = pNombre;
+            Clave = pClave;
+            RFC = pRFC;
+            TipoRegimen = PTipoRegimen;
+            NombreContacto = pNombreComplento;
+
+        }
+
+        public override int Id { get; set; }
+        public  override string Nombre { get; set; }
+        public override string Clave { get; set; }
+
+        public override string RFC { get; set; }
+        public override int TipoRegimen { get; set; }
+
+        public override  string NombreContacto { get; set; }
+
+    }
+}
