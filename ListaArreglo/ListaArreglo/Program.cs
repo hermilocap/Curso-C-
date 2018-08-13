e7b01dc597a5dd1efcba4cd0749c030f2a32fbe1
hermilocap <hermilocap@hotmail.com>
2018-08-12 19:41:42 -0500

    Uso de ArrayList

---
+++ ListaArreglo/ListaArreglo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaArreglo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            clsMostrarArrayList mostrar = new clsMostrarArrayList();
+            mostrar.CapturarDatos();
+            mostrar.ImprimirDatos();
+
+        }
+    }
+}
---
+++ ListaArreglo/ListaArreglo/clsMostrarArrayList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaArreglo
+{
+    class clsMostrarArrayList
+    {
+        ArrayList lista;
+        public clsMostrarArrayList()
+        {
+            lista = new ArrayList();
+        }
+        public void CapturarDatos() {
+            string cadena;
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Dame el dato {0}",i+1);
+                cadena = Console.ReadLine();
+                lista.Add(cadena);
+
+            }
+        }
+        public void ImprimirDatos()
+        {
+            foreach (var item in lista)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
