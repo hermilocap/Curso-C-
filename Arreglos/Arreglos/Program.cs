0f120887d8934d8da8df8c3f1863960aa6e0ccc1
hermilocap <hermilocap@hotmail.com>
2018-08-12 19:01:32 -0500

    Arreglos

---
+++ Arreglos/Arreglos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arreglo = new int[12];
+            int i;
+            for (i = 0; i <Arreglo.Length; i++)
+            {
+                string cadena = Console.ReadLine();
+                Arreglo[i] = Convert.ToInt32(cadena);
+
+               // Arreglo[i] =i+1;
+            }
+
+            for (i = 0; i < Arreglo.Length; i++)
+            {
+                Console.WriteLine("{0}",Arreglo[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
